ebee789f10760f54a04e05841dc1fe3ba3c8b833
erickcion <erickcion@gmail.com>
2010-10-18 16:16:36 -0430

    Commit Inicial

---
+++ instalasite.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+###############################################################################
+# Este programa es Sofware Libre; puedes redistribuirlo y/o modificarlo bajo
+# los terminos de la GNU General Public License como la publica la Free
+# Software Fundation; en la version 2 de la licencia, o (a su elección)
+# cualquer version siguiente.
+
+# Este programa se publica esperando que sea útil, pero SIN NINGUNA GARANTIA;
+# ni siquiera la garantía implícita de COMERCIALIZACIÓN o IDONEIDAD PARA UN
+# PROPÓSITO PARTICULAR. Consulte la GNU General Public License para más
+# detalles.
+###############################################################################
+
+# instalasite.sh -- un programa para instalar facilmente en un servidor
+# aplicaciones web que requieran de una base de datos MySQL
+
+# Creado por: Erick Birbe <erickcion@gmail.com>
